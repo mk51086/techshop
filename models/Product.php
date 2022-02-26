5d80e399ca1425e9b0960bf493bb1beaa76cdc8d
unknown <mk51086@ubt-uni.net>
2022-02-26 23:56:15 +0100

    ­

--- models/Product.php
+++ models/Product.php
@@ -51,6 +51,18 @@
         return $products;
     }
 
+     public function getProduct($id)
+     {
+         $query = "SELECT * FROM " . Product::$table_name . " WHERE ProductID = ?";
+         $stmt = $this->db->conn->prepare($query);
+         $stmt->bindParam(1, $id, PDO::PARAM_INT);
+         $stmt->execute();
+         $product = null;
+         while ($row = $stmt->fetch()) {
+             $product = $this->createProduct($row);
+         }
+         return $product;
+     }
 
 
 
